ad5efa9e9eca803d15f972921e10c0257946e52a
Meghana0927 <94678051+Meghana0927@users.noreply.github.com>
2022-07-28 09:04:42 +0000

    coded

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,12 +1,20 @@
 # Functions
 
 
+
 def computepay(h, r):
-    pass  # ...
+    computepay=0
+    pay=float(computepay)
+    if h<40:
+        pay=(h*r)
+    if h>40:
+        pay=((40*r)+(h-40)*1.5*r)
+    return pay
+    
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate=input("Enter Rate:")
+r=float(rate)
 
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
+p = computepay(h, r)
 print("Pay", p)
--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,8 @@
 # Strings
 
-text = "X-DSPAM-Confidence:    0.8475"
+
+text = "X-DSPAM-Confidence:    0.8475";
+txt = text.find(':')
+number = text[txt+1:]
+n = float(number)
+print(n)
